a8db9e7ca89c161772229da44685363ee3d9fb53
Christian Flach <cmfcmf.flach@gmail.com>
2018-01-10 10:39:41 +0100

    Add cli-test.js

--- source/cli-app/main.cpp
+++ source/cli-app/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char* argv[]) {
     std::cout << "Hello from C++" << std::endl;
     node::lib::Initialize("here_is_the_program_name");
 
-    std::cout << "Running debug.js from C++" << std::endl;
-    node::lib::Run("debug.js");
+    std::cout << "Running cli-test.js from C++" << std::endl;
+    node::lib::Run("cli-test.js");
     while (node::lib::ProcessEvents()) { }
 
     std::cout << "Evaluating stuff from C++" << std::endl;
@@ -19,11 +19,9 @@ int main(int argc, char* argv[]) {
     node::lib::Evaluate("console.log('this is EVEN MORE evaluated!');console.log(christian);");
     while (node::lib::ProcessEvents()) { }
 
-    /*
     auto fs = node::lib::IncludeModule("fs");
     node::lib::Call(fs, "foo");
     while (node::lib::ProcessEvents()) { }
-    */
 
     return 0;
 }
