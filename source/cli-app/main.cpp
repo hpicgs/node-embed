1dc1d6208a62f82a9039015928f328264fdd7650
Johannes Schneider <johannes.schneider@student.hpi.de>
2018-01-11 13:03:33 +0100

    Changed code for demo

--- source/cli-app/main.cpp
+++ source/cli-app/main.cpp
@@ -8,28 +8,17 @@ int main(int argc, char* argv[]) {
     std::string program_name = argc >= 1 ? std::string(argv[0]) : "cli_app";
     node::lib::Initialize(program_name);
 
-    std::cout << "Running cli-test.js from C++" << std::endl;
-    node::lib::Run("cli-test.js");
-    while (node::lib::ProcessEvents()) { }
+    // Example #1: Execute JS code on the fly
+    node::lib::Evaluate("const uuid = require('uuid/v4'); console.log(uuid());");
 
-    std::cout << "Evaluating stuff from C++" << std::endl;
-    node::lib::Evaluate("console.log('this is evaluated!');let christian = 'hmm';");
-
-    std::cout << "Evaluating MORE stuff from C++" << std::endl;
-    node::lib::Evaluate("console.log('this is EVEN MORE evaluated!');console.log(christian);");
-    v8::Isolate *isolate = v8::Isolate::GetCurrent();
-
+    // Example #2: Exchange values between C++ host and JS
+{
+    auto isolate = v8::Isolate::GetCurrent();
     auto fs = node::lib::IncludeModule("fs");
-    // Check if file cli-test.js exists in the current working directory.
     auto result = node::lib::Call(fs, "existsSync", {v8::String::NewFromUtf8(isolate, "cli-test.js")});
     auto file_exists = v8::Local<v8::Boolean>::Cast(result)->BooleanValue();
     std::cout << (file_exists ? "cli-test.js exists in cwd" : "cli-test.js does not exist in cwd") << std::endl;
-
-    auto root = node::lib::GetRootObject();
-    node::lib::Call(root, "eval", {v8::String::NewFromUtf8(isolate, "console.log('1234');")});
-
-    // This would never return and run forever, even if Terminate is called.
-    //node::lib::Evaluate("while (true) {}");
+}
 
     node::lib::Terminate();
 
