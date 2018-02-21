a347cfc3cd38ba9760697f061db9e4a9c131d058
tim <tim@luminosuslight.de>
2018-02-21 11:20:55 +0100

    Complete CLI app refactoring

--- data/node-lib-cli.js
+++ data/node-lib-cli.js
@@ -1,13 +1,14 @@
-console.log('Hi from node-lib-cli.js');
 
-let i = 0;
+let i = 1;
 
-const hello = () => {
-    console.log('Hello again from node-lib-cli.js: ' + i);
+const demoEvent = () => {
+    console.log('[JS ] This is the ' + i + '. event.');
     i++;
-    if (i < 10) {
-        setTimeout(hello, 200);
+    if (i < 4) {
+        setTimeout(demoEvent, 200);
     }
 };
 
-setTimeout(hello, 200);
+setTimeout(demoEvent, 200);
+
+console.log('[JS ] node-lib-cli.js was executed and events were scheduled.');
--- source/node-lib-cli/main.cpp
+++ source/node-lib-cli/main.cpp
@@ -6,7 +6,7 @@
 #include <cpplocate/cpplocate.h>
 
 void cppFunctionExposedToJs(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  std::cout << "This is a C++ function called from JS." << std::endl;
+  std::cout << "[C++] This is a C++ function called from JS." << std::endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -14,35 +14,35 @@ int main(int argc, char* argv[]) {
   const std::string js_file = "data/node-lib-cli.js";
   const std::string data_path = cpplocate::locatePath(js_file);
   if (data_path.empty()) {
-    std::cerr << "Could not find data path." << std::endl;
+    std::cerr << "[C++] Could not find data path." << std::endl;
     return 1;
   }
   const std::string js_path = data_path + "/" + js_file;
 
-  std::cout << "Initializing Node.js instance" << std::endl;
+  std::cout << "[C++] Initializing Node.js instance" << std::endl;
   std::string program_name = argc >= 1 ? std::string(argv[0]) : "cli_app";
-  node::Initialize(program_name);
+  node::Initialize(program_name, {"-e", "console.log()"});
 
-  std::cout << "Running node-lib-cli.js from C++" << std::endl;
+  std::cout << "[C++] Running node-lib-cli.js from C++" << std::endl;
   node::Run(js_path);
-  std::cout << "Processing events in event queue" << std::endl;
+  std::cout << "[C++] Processing events in event queue" << std::endl;
   while (node::ProcessEvents()) { }
 
-  std::cout << "Evaluating a JS string from C++" << std::endl;
-  node::Evaluate("let text = 'This is text is stored in a global variable in JS.';");
+  std::cout << "[C++] Evaluating a JS string from C++" << std::endl;
+  node::Evaluate("let text = '[JS ] This is text is stored in a global variable in JS.';");
   node::Evaluate("console.log(text);");
 
-  std::cout << "Loading the 'fs' NPM module directly from C++" << std::endl;
+  std::cout << "[C++] Loading the 'fs' NPM module directly from C++" << std::endl;
   auto fs = node::IncludeModule("fs");
-  std::cout << "Using 'fs' to check if file node-lib-cli.js exists" << std::endl;
+  std::cout << "[C++] Using 'fs' to check if file node-lib-cli.js exists" << std::endl;
   v8::Isolate* isolate = v8::Isolate::GetCurrent();
   auto result = node::Call(fs.ToLocalChecked(), "existsSync", {v8::String::NewFromUtf8(isolate, js_path.c_str())});
   auto file_exists = v8::Local<v8::Boolean>::Cast(result.ToLocalChecked())->BooleanValue();
-  std::cout << (file_exists ? "node-lib-cli.js exists" : "node-lib-cli.js does not exist") << std::endl;
+  std::cout << (file_exists ? "[C++] node-lib-cli.js exists" : "[C++] node-lib-cli.js does not exist") << std::endl;
 
-  std::cout << "Calling a global JS function from C++" << std::endl;
+  std::cout << "[C++] Calling a global JS function from C++" << std::endl;
   auto root = node::GetRootObject().ToLocalChecked();
-  node::Call(root, "eval", {v8::String::NewFromUtf8(isolate, "console.log('Called a global JS function.');")});
+  node::Call(root, "eval", {v8::String::NewFromUtf8(isolate, "console.log('[JS ] Called a global JS function.');")});
 
   node::RegisterModule("CliDemoModule", {
                          {"cppFunctionExposedToJs", cppFunctionExposedToJs},
