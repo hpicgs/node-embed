dfb804b5a6bccca3eef480611725a0be92614c1b
Christian Flach <cmfcmf.flach@gmail.com>
2018-02-25 17:26:38 +0100

    Whoops - remove debug code

--- data/node-lib-qt-rss.js
+++ data/node-lib-qt-rss.js
@@ -3,8 +3,8 @@ var request = require('request'); // for fetching the feed
 
 var emitRequest = function () {
   console.log("Refreshing feeds...")
-  var feedparser = new FeedParser([]);  
-  var req = request('http://feeds.bbci.co.uk/news/world/rss.xml', {timeout: 3});
+  var feedparser = new FeedParser([]);
+  var req = request('http://feeds.bbci.co.uk/news/world/rss.xml');
 
   req.on('error', function (error) {
     // handle any request errors
@@ -12,7 +12,7 @@ var emitRequest = function () {
 
   req.on('response', function (res) {
     var stream = this; // `this` is `req`, which is a stream
-  
+
     if (res.statusCode !== 200) {
       this.emit('error', new Error('Bad status code'));
     }
@@ -21,17 +21,17 @@ var emitRequest = function () {
       stream.pipe(feedparser);
     }
   });
- 
+
   feedparser.on('error', function (error) {
     // catch all parser errors but don't handle them
   });
-  
+
   feedparser.on('readable', function () {
     // This is where the action is!
     var stream = this; // `this` is `feedparser`, which is a stream
     var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
     var item;
-  
+
     var itemString = '';
     while (item = stream.read()) {
         itemString = itemString + item['title'] + '\n' + item['description'];
