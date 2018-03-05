aa17531cab4bd8b5fa101d4a2b5339af2b6df3e6
Christian Flach <cmfcmf.flach@gmail.com>
2018-03-05 11:59:44 +0100

    Simplify JS code

--- data/node-lib-qt-rss.js
+++ data/node-lib-qt-rss.js
@@ -1,42 +1,21 @@
 var FeedParser = require('feedparser');
-var request = require('request'); // for fetching the feed
+var request = require('request');
 
-var emitRequest = function () {
-  console.log("Refreshing feeds...")
-  var feedparser = new FeedParser([]);
+var feedparser = new FeedParser();
+
+feedparser.on('readable', function () {
+  var item = this.read();
+
+  if (item && item['title']) {
+    cppQtGui.addFeedItem(item);
+  }
+});
+
+function loadBBCFeed() {
   var req = request('http://feeds.bbci.co.uk/news/world/rss.xml');
 
-  req.on('error', function (error) {
-    // catch all request errors but don't handle them in this demo
-  });
-
   req.on('response', function (res) {
-    var stream = this; // `this` is `req`, which is a stream
-
-    if (res.statusCode !== 200) {
-      this.emit('error', new Error('Bad status code'));
-    }
-    else {
-      cppQtGui.clearFeed();
-      stream.pipe(feedparser);
-    }
-  });
-
-  feedparser.on('error', function (error) {
-    // catch all parser errors but don't handle them in this demo
-  });
-
-  feedparser.on('readable', function () {
-    var stream = this; // `this` is `feedparser`, which is a stream
-    var item = stream.read();
-
-    if (item && item['title']) {
-      var itemString = item['title'] + '\n' + item['description'];
-      cppQtGui.addFeedItem( { item: itemString } );
-    }
-  });
-
-  feedparser.on('end', function (){
-    cppQtGui.redraw();
+    cppQtGui.clearFeed();
+    this.pipe(feedparser);
   });
 }
